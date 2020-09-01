b6634c275c2cf000deacdd349a02fddc516c910b
bipin gaire <bpngaire@gmail.com>
2020-09-01 12:39:00 +0545

    .gitignore

--- gcd.js
+++ gcd.js
@@ -1,15 +1,15 @@
-// function gcd(num1, num2) {
-//     while (num1 !== num2) {
-//         if (num1 > num2) {
-//             num1 = num1 - num2
-//         } else {
-//            num2 = num2 - num1
-//         }
-//     }
-//     return num1;
-// }
+function gcd(num1, num2) {
+    while (num1 !== num2) {
+        if (num1 > num2) {
+            num1 = num1 - num2
+        } else {
+           num2 = num2 - num1
+        }
+    }
+    return num1;
+}
 
-// console.log("gretest common factor is :", gcd(6, 12))
+console.log("gretest common factor is :", gcd(6, 12))
 
 /**********************************************************/
 // OPTIMIZED PROGRAM 
