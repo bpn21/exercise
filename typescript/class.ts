d0e3d0aabf5e587b45e67882ccbe6820b2b1dcac
bipin gaire <bpngaire@gmail.com>
2020-09-18 23:52:53 +0545

    angular

--- typescript/class.ts
+++ typescript/class.ts
@@ -1,1 +1,23 @@
-console.log('hello')
+class fruits {
+    constructor() {
+        let origin = 'Nepal'
+        let type = 'Organic'
+        console.log('object created :')
+        console.log('origin is :', origin)
+        console.log('type is :', type)
+
+    }
+
+    name(name: String) {
+        console.log(`name of fruit is ${name}`)
+    }
+
+    price(name: String, price: Number) {
+        console.log(`price of ${name} is ${price}`)
+
+    }
+}
+
+const apple = new fruits;
+apple.name('apple');
+apple.price('apple', 100)
