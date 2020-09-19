74ecbfff8fa1034ad16dbe3b7775a0be5ea69e4f
bipin gaire <bpngaire@gmail.com>
2020-09-19 21:30:15 +0545

    spereration of concern

--- notes/javascript/function/functionalConstructor.js
+++ notes/javascript/function/functionalConstructor.js
@@ -6,51 +6,53 @@
 // //3) (important) functional Constructor is always called with keyword NEW.
 
 // // console.log('this is outside', this)
-// function Fruits(name, country, price) {
-//     // console.log('thi is inside', this);
+function Fruits(name, country, price) {
+    // console.log('thi is inside', this);
+    this.name = name;
+    this.origin = country;
+    this.price = 'Rs ' + price;
+}
+
+var apple = new Fruits('Apple', 'Nepal', 110);
+console.log("apple.origin", apple.origin)
+var mango = new Fruits();
+console.log("mango origin", mango.origin = 'china') //
+var orange = new Fruits();
+var papaya = new Fruits();
+var banana = new Fruits();
+
+
+console.log("type of ", typeof (apple))  // object , object Fruits { name: undefined, origin: 'china', price: 'Rs undefined' }
+console.log("type of ",typeof (mango));
+console.log(orange);
+console.log(papaya);
+console.log(banana);
+
+
+
+
+// function Vegitable(name, origin, price) {
 //     this.name = name;
-//     this.origin = country;
-//     this.price = 'Rs ' + price;
+//     this.origin = origin;
+//     this.price = price;
 // }
 
-// var apple = new Fruits('Apple', 'Nepal', 110);
-// var mango = new Fruits();
-// var orange = new Fruits();
-// var papaya = new Fruits();
-// var banana = new Fruits();
+// spanish = new Vegitable('spanish', 'america', 'Rs 50');
+// kauliflower = new Vegitable();
+// cabbage = new Vegitable();
+// ladiesfinger = new Vegitable();
+// pumpkin = new Vegitable();
+// bringle = new Vegitable();
 
 
-// console.log(apple)
-// console.log(mango);
-// console.log(orange);
-// console.log(papaya);
-// console.log(banana);
+// //  prototype is keyboard to add property method to fucntional constructor.
+// Vegitable.prototype.color = 'green'; //here all the vegitables become green ! 
+// // newly added property.. but value for that property will be same for all the objects
+// console.log(spanish.color);
 
-
-
-
-function Vegitable(name, origin, price) {
-    this.name = name;
-    this.origin = origin;
-    this.price = price;
-}
-
-spanish = new Vegitable('spanish', 'america', 'Rs 50');
-kauliflower = new Vegitable();
-cabbage = new Vegitable();
-ladiesfinger = new Vegitable();
-pumpkin = new Vegitable();
-bringle = new Vegitable();
-
-
-//  prototype is keyboard to add property method to fucntional constructor.
-Vegitable.prototype.color = 'green'; //here all the vegitables become green ! 
-// newly added property.. but value for that property will be same for all the objects
-console.log(spanish.color);
-
-Vegitable.prototype.size = function (size) {
-    return size;
-}
-console.log(kauliflower.size('big'));
-console.log(spanish);
-console.log(kauliflower);
+// Vegitable.prototype.size = function (size) {
+//     return size;
+// }
+// console.log(kauliflower.size('big'));
+// console.log(spanish);
+// console.log(kauliflower);
